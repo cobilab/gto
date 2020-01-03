c7298de3499657e160a4596221bcf4da265cf70e
Diogo Pratas <pratas@ua.pt>
2020-01-03 10:37:36 +0000

    fix

--- pipelines/gto_metagenomics.sh
+++ pipelines/gto_metagenomics.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
-#
 # ==============================================================================
 # |                                                                            |
 # |             THIS PROGRAM INFERS METAGENOMIC COMPOSITION W/ GTO             |
 # |             ==================================================             |
 # |                                                                            |
-# |                  ./gto_metagenomics.sh db.fa reads.fq                      |
+# |              ./gto_metagenomics.sh reads1.fq reads2.fq VDB.fa              |
 # |                                                                            |
 # |                     FILE NEEDED TO THE COMPUTATION:                        |
 # |                                                                            |
-# |                 $1: FILE WITH THE DATABASE IN FASTA FORMAT                 |
-# |                 $2: FILE WITH THE READS IN FASTQ FORMAT                    |
+# |                 $3: FILE WITH THE DATABASE IN FASTA FORMAT                 |
+# |                 $2: FILE WITH THE READS #2 IN FASTQ FORMAT                 |
+# |                 $1: FILE WITH THE READS #1 IN FASTQ FORMAT                 |
 # |                                                                            |
 # ==============================================================================
 #
 # ==============================================================================
 # ================================ DEFINITIONS =================================
-#
-LEVEL="$2";
 #
 GET_GTO=0;
 RUN_COMPARISON=1;
@@ -36,11 +34,15 @@ if [[ "$GET_GTO" -eq "1" ]];
 if [[ "$RUN_COMPARISON" -eq "1" ]];
   then
   #
+  gto_fastq_minimum_local_quality_score_forward -k 5 -w 25 -m 33 < $1 \
+  | gto_fastq_exclude_n -m 5 \
+  | gto_fastq_minimum_read_size -s 30 > filt-$1
+  #
   gto_fastq_minimum_local_quality_score_forward -k 5 -w 25 -m 33 < $2 \
   | gto_fastq_exclude_n -m 5 \
   | gto_fastq_minimum_read_size -s 30 > filt-$2
   #
-  gto_fastq_falcon -n 4 filt-$2 $1
+  gto_fastq_metagenomics -F -t 200 -v -n 8 filt-$1:filt-$2 $3
   #
   fi
 #
