cdd2589450b10326e2fa84a3d6a3168afc5e5a92
Pratas <pratas@ua.pt>
2020-06-09 19:19:39 +0100

    info messages in pipeline

--- pipelines/gto_create_sars_dataset/gto_create_sars_dataset.sh
+++ pipelines/gto_create_sars_dataset/gto_create_sars_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+echo "Downloading data ...";
 efetch -db nucleotide -format fasta -id "AY386330.1" > B19.fa
 efetch -db nucleotide -format fasta -id "JN561323.2" > HV2.fa
 efetch -db nucleotide -format fasta -id "X04370.1"   > HV3.fa
@@ -9,19 +10,24 @@ efetch -db nucleotide -format fasta -id "MG921180.1" > HPV.fa
 efetch -db nucleotide -format fasta -id "NC_012920.1" > MT.fa
 #
 # SUBSTITUTIONS
+echo "Applying SNPs ...";
 gto_fasta_mutate -e 0.01 < MT.fa  > MT_1.fa
 gto_fasta_mutate -e 0.01 < B19.fa > B19_1.fa
 gto_fasta_mutate -e 0.05 < TTV.fa > TTV_5.fa
 gto_fasta_mutate -e 0.05 < HPV.fa > HPV_5.fa
 #
 # PERMUTATIONS
+echo "Applying permutations ...";
 gto_permute_by_blocks -b 500 -s 0 < B19.fa > B19_P.fa
 gto_permute_by_blocks -b 300 -s 0 < HPV_5.fa > HPV_5_P.fa
 gto_permute_by_blocks -b 100 -s 0 < HV2.fa > HV2_P.fa
 #
+echo "Simulating trash dna ...";
 gto_genomic_gen_random_dna -n 300000 -s 0 | gto_fasta_from_seq -l 100 > trash.fa
 #
+echo "Joinning data ...";
 cat trash.fa MT_1.fa B19_P.fa HPV_5_P.fa B19_1.fa TTV_5.fa SARS2.fa HV3.fa > data.fa
 #
-art_illumina -ss HS25 -sam -i data.fa -p -l 150 -f 30 -m 200 -s 10 -o reads
+echo "Simulating reads ...";
+art_illumina -ss HS25 -sam -i data.fa -p -l 150 -f 30 -m 200 -s 10 -o reads 1>> art_info
 #
