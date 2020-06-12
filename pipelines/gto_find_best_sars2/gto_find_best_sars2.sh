6ede0fa2fc2d9290b38f0772b8e9489d2e151466
Pratas <pratas@ua.pt>
2020-06-12 12:34:00 +0100

    fix name

--- pipelines/gto_find_best_sars2/gto_find_best_sars2.sh
+++ pipelines/gto_find_best_sars2/gto_find_best_sars2.sh
@@ -3,4 +3,4 @@ gto_create_sars_dataset.sh # PE reads with MT007544 SARS-CoV-2
 #
 gto_download_sars2.sh # 93 SARS-CoV-2 genome sequences
 #
-gto_fastq_metagenomics -v -F -l 47 reads1.fq:reads2.fq SARS-Cov-2.fa
+gto_fastq_metagenomics -v -F -l 47 reads1.fq:reads2.fq SARS-CoV-2.fa
