91ae2a3f8739ec9f553601dd69d0205a3a2d682c
João Rafael Almeida <joao.rafael.almeida@ua.pt>
2021-02-22 12:23:21 +0000

    Improve gto_map_rearrangements

--- pipelines/gto_map_rearrangements/gto_map_rearrangements.sh
+++ pipelines/gto_map_rearrangements/gto_map_rearrangements.sh
@@ -9,52 +9,187 @@
 # |                                                                            |
 # |                     FILE NEEDED TO THE COMPUTATION:                        |
 # |                                                                            |
-# |                $1: FILE WITH THE GENOME #1 IN FASTA FORMAT                 |
-# |                $2: FILE WITH THE GENOME #2 IN FASTA FORMAT                 |
+# |             REF:   $1: FILE WITH THE GENOME #1 IN FASTA FORMAT             |
+# |             CON:   $2: FILE WITH THE GENOME #2 IN FASTA FORMAT             |
 # |                                                                            |
 # ==============================================================================
 #
 # ==============================================================================
 # ================================ DEFINITIONS =================================
 #
-LEVEL="$2";
-#
-GET_GTO=0;
-RUN_COMPARISON=1;
 PARAM=" -rm 13:50:0:0:0.9/0:10:0.94 ";
 PARAM_R=" -rm 13:50:2:0:0.9/0:10:0.94 ";
+WINDOW="5";
+REFERENCE="";
+TARGET="";
+THRESHOLD="1.5";
+LINK="5";
+WIDTH="30";
+SPACE="120";
+RUN="0";
+# 
+# ==============================================================================
+#
+SHOW_MENU () {
+  echo " -------------------------------------------------------   ";
+  echo "                                                           ";
+  echo " gto_map_rearrangements.sh >> maps and creates an image    ";
+  echo "                              w possible rearrangements    ";
+  echo "                                                           ";
+  echo " Program options ---------------------------------------   ";
+  echo "                                                           ";
+  echo " -h, --help                      Show this,                ";
+  echo " -c, --install                   Installation (conda),     ";
+  echo "                                                           ";
+  echo " -w <INT>, --window <INT>        Low-pass filter size,     ";
+  echo " -th <REAL>, --threshold <REAL>  Segmentation threshold,   ";
+  echo "                                                           ";
+  echo " -l <INT>, --link <INT>          Link type [0;4],          ";
+  echo " -w <INT>, --width <INT>         Width size,               ";
+  echo " -s <INT>, --space <INT>         Space size.               ";
+  echo "                                                           ";
+  echo " Input options -----------------------------------------   ";
+  echo "                                                           ";
+  echo " -r <FILE>, --reference <FILE>   FASTA Ref. filename,      ";
+  echo " -t <FILE>, --target <FILE>      FASTA target filename     ";
+  echo "                                 (represented with CON)    ";
+  echo "                                                           ";
+  echo " Example -----------------------------------------------   ";
+  echo "                                                           ";
+  echo " gto_map_rearrangements.sh --window 5 --target CON.fa \\   ";
+  echo " --reference REF.fa                                        ";
+  echo "                                                           ";
+  echo " -------------------------------------------------------   ";
+  }
 #
 # ==============================================================================
-# ================================== GET GTO ===================================
 #
-if [[ "$GET_GTO" -eq "1" ]];
+CHECK_INPUT () {
+  FILE=$1
+  if [ -f "$FILE" ];
+    then
+    echo "Input filename: $FILE"
+    else
+    echo -e "\e[31mERROR: input file not found ($FILE)!\e[0m";
+    SHOW_MENU;
+    exit;
+    fi
+  }
+#
+# ==============================================================================
+#
+PROGRAM_EXISTS () {
+  if ! [ -x "$(command -v $1)" ];
+    then
+    echo -e "\e[41mERROR\e[49m: $1 is not installed." >&2;
+    echo -e "\e[42mTIP\e[49m: Try: gto_map_rearrangements.sh --install" >&2;
+    exit 1;
+    fi
+  }
+#
+# ==============================================================================
+#
+if [[ "$#" -lt 1 ]];
   then
-  conda install -c cobilab gto --yes
+  HELP=1;
+  fi
+#
+POSITIONAL=();
+#
+while [[ $# -gt 0 ]]
+  do
+  i="$1";
+  case $i in
+    -h|--help|?)
+      HELP=1;
+      shift
+    ;;
+    -c|--install|--compile)
+      INSTALL=1;
+      shift
+    ;;
+    -r|--reference)
+      REFERENCE="$2";
+      RUN=1;
+      shift 2;
+    ;;
+    -th|--threshold)
+      THRESHOLD="$2";
+      shift 2;
+    ;;
+    -t|--target)
+      TARGET="$2";
+      shift 2;
+    ;;
+    -w|--width)
+      WIDTH="$2";
+      shift 2;
+    ;;
+    -l|--link)
+      LINK="$2";
+      shift 2;
+    ;;
+    -s|--space)
+      SPACE="$2";
+      shift 2;
+    ;;
+    -w|--window)
+      WINDOW="$2";
+      shift 2;
+    ;;
+    -*) # unknown option with small
+    echo "Invalid arg ($1)!";
+    echo "For help, try: gto_map_rearrangements.sh -h"
+    exit 1;
+    ;;
+  esac
+  done
+#
+set -- "${POSITIONAL[@]}" # restore positional parameters
+#
+# ==============================================================================
+#
+if [[ "$HELP" -eq "1" ]];
+  then
+  SHOW_MENU;
+  exit;
   fi
 #
 # ==============================================================================
-# =============================== RUN COMPARISON ===============================
 #
-if [[ "$RUN_COMPARISON" -eq "1" ]];
+if [[ "$INSTALL" -eq "1" ]];
   then
-  gto_fasta_rand_extra_chars < $1 | gto_fasta_to_seq > SEQ1;
-  gto_fasta_rand_extra_chars < $2 | gto_fasta_to_seq > SEQ2;
+  conda install -c cobilab gto --yes
+  exit;
+  fi
+#
+# ==============================================================================
+#
+if [[ "$RUN" -eq "1" ]];
+  then
+  #
+  PROGRAM_EXISTS "gto";
+  #
+  CHECK_INPUT "$REFERENCE";
+  CHECK_INPUT "$TARGET";
+  #
+  gto_fasta_rand_extra_chars < $REFERENCE | gto_fasta_to_seq > SEQ1;
+  gto_fasta_rand_extra_chars < $TARGET | gto_fasta_to_seq > SEQ2;
   #
   # GET WINDOW SIZE BY SEQUENCE SIZE FOR 1
   LENGTH1=`gto_info < SEQ1 | grep "Number of sym" | awk '{ print $5}'`;
   LENGTH2=`gto_info < SEQ2 | grep "Number of sym" | awk '{ print $5}'`;
   #
-  echo "#SCF $LENGTH1 $LENGTH2" > rearrangements-$1-$2.csv;
+  echo "#SCF $LENGTH2 $LENGTH1" > rearrangements-$REFERENCE-$TARGET.csv;
   #
-  WINDOW_SIZE=`echo "$LENGTH1 / 1000" | bc`;
-  echo "WINDOW SIZE: $WINDOW_SIZE";
+  echo "WINDOW SIZE: $WINDOW";
   #
   gto_geco -v $PARAM -e -r SEQ1 SEQ2
   gto_upper_bound -u 2 < SEQ2.iae > SEQ_2_UB
-  gto_filter -d 0 -w $WINDOW_SIZE -c < SEQ_2_UB > PROFILE_N2
-  gto_segment -t 1.2 < PROFILE_N2 > $2.positions
+  gto_filter -d 0 -w $WINDOW -c < SEQ_2_UB > PROFILE_N2
+  gto_segment -t $THRESHOLD < PROFILE_N2 > $TARGET.positions
   #
-  mapfile -t LINES < $2.positions
+  mapfile -t LINES < $TARGET.positions
   for line in "${LINES[@]}" # 
     do
     #
@@ -65,8 +200,8 @@ if [[ "$RUN_COMPARISON" -eq "1" ]];
     #
     gto_geco -v $PARAM -e -r REGION.g SEQ1
     gto_upper_bound -u 2 < SEQ1.iae > SEQ1-UB
-    gto_filter -d 0 -w $WINDOW_SIZE -c < SEQ1-UB > PROFILE-SEQ1-$I_POS-$E_POS
-    gto_segment -t 1.2 < PROFILE-SEQ1-$I_POS-$E_POS > tmp-positions.csv
+    gto_filter -d 0 -w $WINDOW -c < SEQ1-UB > PROFILE-SEQ1-$I_POS-$E_POS
+    gto_segment -t $THRESHOLD < PROFILE-SEQ1-$I_POS-$E_POS > tmp-positions.csv
     #
     mapfile -t TMP < tmp-positions.csv
     #
@@ -76,7 +211,7 @@ if [[ "$RUN_COMPARISON" -eq "1" ]];
       I_TMP=`echo $y | tr ':' '\t' | awk '{ print $1 }'`;
       E_TMP=`echo $y | tr ':' '\t' | awk '{ print $2 }'`;
       #
-      printf "r\t%u\t%u\t%u\t%u\tt\t%u\t%u\t%u\t%u\n" "$I_POS" "$E_POS" "$I_POS" "$E_POS" "$I_TMP" "$E_TMP" "$I_TMP" "$E_TMP" >> rearrangements-$1-$2.csv
+      printf "r\t%u\t%u\t%u\t%u\tt\t%u\t%u\t%u\t%u\n" "$I_POS" "$E_POS" "$I_POS" "$E_POS" "$I_TMP" "$E_TMP" "$I_TMP" "$E_TMP" >> rearrangements-$REFERENCE-$TARGET.csv
       #
       done
     #
@@ -86,10 +221,10 @@ if [[ "$RUN_COMPARISON" -eq "1" ]];
   #
   gto_geco -v $PARAM_R -e -r SEQ1 SEQ2
   gto_upper_bound -u 2 < SEQ2.iae > SEQ_2_UB
-  gto_filter -d 0 -w $WINDOW_SIZE -c < SEQ_2_UB > PROFILE_N2
-  gto_segment -t 1.2 < PROFILE_N2 > $2.positions
+  gto_filter -d 0 -w $WINDOW -c < SEQ_2_UB > PROFILE_N2
+  gto_segment -t $THRESHOLD < PROFILE_N2 > $TARGET.positions
   #
-  mapfile -t LINES < $2.positions
+  mapfile -t LINES < $TARGET.positions
   for line in "${LINES[@]}" # 
     do
     #
@@ -100,8 +235,8 @@ if [[ "$RUN_COMPARISON" -eq "1" ]];
     #
     gto_geco -v $PARAM_R -e -r REGION.g SEQ1
     gto_upper_bound -u 2 < SEQ1.iae > SEQ1-UB
-    gto_filter -d 0 -w $WINDOW_SIZE -c < SEQ1-UB > PROFILE-SEQ1-$I_POS-$E_POS
-    gto_segment -t 1.2 < PROFILE-SEQ1-$I_POS-$E_POS > tmp-positions.csv
+    gto_filter -d 0 -w $WINDOW -c < SEQ1-UB > PROFILE-SEQ1-$I_POS-$E_POS
+    gto_segment -t $THRESHOLD < PROFILE-SEQ1-$I_POS-$E_POS > tmp-positions.csv
     #
     mapfile -t TMP < tmp-positions.csv
     #
@@ -111,13 +246,13 @@ if [[ "$RUN_COMPARISON" -eq "1" ]];
       I_TMP=`echo $y | tr ':' '\t' | awk '{ print $1 }'`;
       E_TMP=`echo $y | tr ':' '\t' | awk '{ print $2 }'`;
       #
-      printf "r\t%u\t%u\t%u\t%u\tt\t%u\t%u\t%u\t%u\n" "$I_POS" "$E_POS" "$I_POS" "$E_POS" "$E_TMP" "$I_TMP" "$E_TMP" "$I_TMP" >> rearrangements-$1-$2.csv
+      printf "r\t%u\t%u\t%u\t%u\tt\t%u\t%u\t%u\t%u\n" "$I_POS" "$E_POS" "$I_POS" "$E_POS" "$E_TMP" "$I_TMP" "$E_TMP" "$I_TMP" >> rearrangements-$REFERENCE-$TARGET.csv
       #
       done
     #
     done
   #
-  gto_comparative_map -o map.svg rearrangements-$1-$2.csv
+  gto_comparative_map -s $SPACE -w $WIDTH -l $LINK -o map.svg rearrangements-$REFERENCE-$TARGET.csv
   #
   fi
 #
